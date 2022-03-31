c691ddaf0e792a8440643bbd26a4b1bb45524335
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-31 18:22:17 -0500

    Comment module and routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all posts
 router.get('/', (req, res) => {
     Post.findAll({
+        order: [['created_at', 'DESC']],
         attributes: ['id', 'content', 'title', 'created_at'],
-        order: [['created_at', 'DESC']], 
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
--- models/index.js
+++ models/index.js
@@ -1,12 +1,25 @@
-const User = require('./User');
+const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment")
 
-User.hasMany(Post, {
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-});
+    foreignKey: 'user_id'
+})
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Comment };
